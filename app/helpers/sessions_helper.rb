d83cc4ff56ebd8661b336b5eb47127b370fb3042
Kai Yi Lee <kaiyisg@yahoo.com.sg>
2017-07-07 16:53:13 -0700

    Adding logout functionality

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,15 +21,23 @@ module SessionsHelper
     !current_user.nil?
   end
 
+
   def log_out
+    forget current_user
     session.delete(:user_id)
     @current_user = nil
   end
 
-   # Remembers a user in a persistent session.
-   def remember(user)
-     user.remember
-     cookies.permanent.signed[:user_id] = user.id
-     cookies.permanent[:remember_token] = user.remember_token
+  # Remembers a user in a persistent session.
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+   def forget(user)
+     user.forget
+     cookies.delete(:user_id)
+     cookies.delete(:remember_token)
    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -6,8 +8,6 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-
-  attr_accessor :remember_token
 
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
@@ -29,5 +29,9 @@ class User < ApplicationRecord
   # Returns true if the given token matches the digest.
   def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
